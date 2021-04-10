f96745018dad91ff3acfbde9948f4275a24c9030
Clifford Okpai <meetmancliff@yahoo.com>
2021-04-10 01:29:12 -0500

    project update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -83,15 +83,15 @@ export default function Projects() {
           <CenterItem>
             <Title>Cliff Blog</Title>
 
-            <a
+            <Link
               target='_blank'
-              href='http://ec2-15-223-46-226.ca-central-1.compute.amazonaws.com/'
+              to='//ec2-15-223-46-226.ca-central-1.compute.amazonaws.com/'
             >
               <div className='ui animated fade button' tabindex='0'>
                 <div className='visible content'>See Project</div>
                 <div className='hidden content'>Explore</div>
               </div>
-            </a>
+            </Link>
             <Text>
               A full stack Blog website with a React front-end, NodeJS and
               MongoDB back-end, and hosted on Amazon AWS.
