be82076121bc7fb5c8502a5260473174fc2fabfe
Clifford Okpai <meetmancliff@gmail.com>
2026-08-16 00:01:52 -0600

    Updates

---
+++ static/js/main.54d1d73c.chunk.js
@@ -0,0 +1,211 @@
+(this["webpackJsonpmy-website"]=this["webpackJsonpmy-website"]||[]).push([[0],{25:function(e,t,i){},37:function(e,t,i){"use strict";i.r(t);i(1);var s=i(17),a=i.n(s),c=(i(25),i(4)),n=i(3),r=i(2),d=i(0);const l=r.a.div`
+  background: radial-gradient(#d3d8a7, #64ac76, #413670);
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,o=r.a.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,j=r.a.p`
+  font-family: 'Source Code Pro', monospace;
+  font-size: 2rem;
+  text-align: center;
+  padding: 0 30px;
+
+  @media screen and (max-width: 1571px) {
+    font-size: 1.8rem;
+  }
+
+  @media screen and (max-width: 1224px) {
+    font-size: 1.7rem;
+  }
+
+  @media screen and (max-width: 1074px) {
+    font-size: 1.6rem;
+  }
+
+  @media screen and (max-width: 933px) {
+    font-size: 1.5rem;
+  }
+
+  @media screen and (max-width: 933px) {
+    font-size: 1.4rem;
+  }
+
+  @media screen and (max-width: 742px) {
+    font-size: 1.3rem;
+  }
+
+  @media screen and (max-width: 627px) {
+    font-size: 1.2rem;
+  }
+
+  @media screen and (max-width: 533px) {
+    font-size: 1.1rem;
+  }
+
+  @media screen and (max-width: 448px) {
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 379px) {
+    font-size: 0.9rem;
+  }
+
+  @media screen and (max-width: 320px) {
+    font-size: 0.7rem;
+  }
+`;var h=function(){return Object(d.jsx)(l,{children:Object(d.jsx)(o,{children:Object(d.jsxs)(j,{children:["Hi, my name is Clifford Okpai and I am a front-end web developer.",Object(d.jsx)("br",{}),Object(d.jsx)("br",{})," When I was a teen, I wasn't quite sure what career path to choose as I thought of becoming an Engineer, a doctor, a pilot, a geologist. When I was in high school, Engineering was on top of that list and I was fond decoupling my transistor radios and handheld games, just out of curiosity, to see what's inside that make them work. As you can imagine, it didn't always end well for young Clifford, as he may not be able to recouple the parts. ",Object(d.jsx)("br",{}),Object(d.jsx)("br",{}),'Prior to the year that I would gain admission into the University of Manitoba, I did a 1 year advanced level program where I did Geography as major. At this point, I still was not sure where I was headed. In my first term at the University of Manitoba, I had the choice of taking a few course as electives and I decided to try the "Introductory Computer Science" course. I was blown away by what I learned in that course and amazed at the things I was able to build by just coding. Software development became my new passion and I never looked back.']})})})};const b=r.a.body`
+  background: radial-gradient(#d3d8a7, #64ac76, #413670);
+  height: 85vh;
+`,x=r.a.div`
+  margin-top: 5%;
+`,m=r.a.h2`
+  width: 100%;
+  text-align: center;
+`;var O=function(){return Object(d.jsx)(b,{children:Object(d.jsx)(x,{children:Object(d.jsxs)("div",{className:"ui container",children:[Object(d.jsx)(m,{children:"Percentages are a reflection of my confidence level when working with these technologies."}),Object(d.jsxs)("div",{className:"ui teal active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"86%"},children:Object(d.jsx)("div",{className:"progress",children:"86%"})}),Object(d.jsx)("div",{className:"label",children:"HTML"})]}),Object(d.jsxs)("div",{className:"ui black active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"89%",color:"white"},children:Object(d.jsx)("div",{className:"progress",children:"89%"})}),Object(d.jsx)("div",{className:"label",children:"CSS"})]}),Object(d.jsxs)("div",{className:"ui purple active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"80%",color:"white"},children:Object(d.jsx)("div",{className:"progress",children:"80%"})}),Object(d.jsx)("div",{className:"label",children:"JavaScript"})]}),Object(d.jsxs)("div",{className:"ui brown active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"78%",color:"white"},children:Object(d.jsx)("div",{className:"progress",children:"78%"})}),Object(d.jsx)("div",{className:"label",children:"React JS"})]}),Object(d.jsxs)("div",{className:"ui pink  active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"50%"},children:Object(d.jsx)("div",{className:"progress",children:"50%"})}),Object(d.jsx)("div",{className:"label",children:"Node JS"})]}),Object(d.jsxs)("div",{className:"ui orange active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"65%"},children:Object(d.jsx)("div",{className:"progress",children:"65%"})}),Object(d.jsx)("div",{className:"label",children:"Redux"})]}),Object(d.jsxs)("div",{className:"ui blue active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"55%"},children:Object(d.jsx)("div",{className:"progress",children:"55%"})}),Object(d.jsx)("div",{className:"label",children:"TypeScript"})]}),Object(d.jsxs)("div",{className:"ui  active violet progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"20%"},children:Object(d.jsx)("div",{className:"progress",children:"20%"})}),Object(d.jsx)("div",{className:"label",children:"GO"})]}),Object(d.jsxs)("div",{className:"ui grey active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"60%"},children:Object(d.jsx)("div",{className:"progress",children:"60%"})}),Object(d.jsx)("div",{className:"label",children:"Java"})]}),Object(d.jsxs)("div",{className:"ui olive  active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"80%"},children:Object(d.jsx)("div",{className:"progress",children:"80%"})}),Object(d.jsx)("div",{className:"label",children:"SASS"})]}),Object(d.jsxs)("div",{className:"ui green active progress",children:[Object(d.jsx)("div",{className:"bar",style:{width:"50%"},children:Object(d.jsx)("div",{className:"progress",children:"50%"})}),Object(d.jsx)("div",{className:"label",children:"MongoDB"})]})]})})})};const p=r.b`
+0%{
+  font-size: 0rem;
+}
+100%{
+  font-size: 5rem;
+}
+`,u=r.b`
+0%{
+  font-size: 0rem;
+}
+100%{
+  font-size: 3rem;
+
+}
+`,v=r.a.div`
+  background: radial-gradient(#d3d8a7, #64ac76, #413670);
+  height: 90vh;
+`,f=r.a.h1`
+  color: rgb(52, 109, 172);
+  font-family: 'Montserrat', sans-serif;
+  font-size: 5rem;
+  animation: 3s ${p} ease-in;
+  text-align: center;
+
+  @media screen and (max-width: 540px) {
+    font-size: 4rem;
+    animation: none;
+  }
+  @media screen and (max-width: 375px) {
+    font-size: 3rem;
+    animation: none;
+  }
+`,g=r.a.h3`
+  color: rgb(78, 80, 80);
+  font-family: 'Dancing Script', cursive;
+  font-size: 3rem;
+  animation: 3s ${u} ease-in;
+  text-align: center;
+
+  @media screen and (max-width: 540px) {
+    font-size: 3rem;
+    animation: none;
+  }
+  @media screen and (max-width: 375px) {
+    font-size: 2rem;
+    animation: none;
+  }
+`,y=r.a.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;var w=function(){return Object(d.jsx)(v,{children:Object(d.jsx)(y,{children:Object(d.jsxs)("div",{children:[Object(d.jsx)(f,{children:"Hello, and welcome!"}),Object(d.jsx)(g,{children:"My name is"}),Object(d.jsx)(f,{children:"Clifford Okpai"}),Object(d.jsx)(g,{children:"and I am a"}),Object(d.jsx)(f,{children:"Front-End Web Developer"})]})})})};const N=r.b`
+0%{
+  opacity: 0;
+}
+100%{
+  opacity: 1;
+}
+`,k=r.a.nav`
+  height: 10vh;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: rgb(78, 80, 80);
+`,z=r.a.h2`
+  animation: 5s ${N} ease-in;
+  color: white;
+
+  &:hover {
+    padding-top: 10px;
+    color: #64ac76;
+  }
+
+  @media screen and (max-width: 414px) {
+    font-size: 16px;
+  }
+`;var I=function(){return Object(d.jsxs)(k,{children:[Object(d.jsx)(c.b,{to:"/",children:Object(d.jsx)(z,{children:Object(d.jsx)("i",{className:"home icon"})})}),Object(d.jsx)(c.b,{to:"/about",children:Object(d.jsx)(z,{children:"About"})}),Object(d.jsx)(c.b,{to:"/projects",children:Object(d.jsx)(z,{children:"Projects"})}),Object(d.jsx)(c.b,{to:"/tech-summary",children:Object(d.jsx)(z,{children:"Tech-Summary"})})]})};const S=r.a.div`
+  position: fixed;
+  height: 5vh;
+  bottom: 0;
+  width: 100%;
+  background-color: rgb(78, 80, 80);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,A=r.a.button`
+  @media screen and (max-width: 333px) {
+    width: 100px;
+
+    i {
+      display: none;
+    }
+  }
+`;function C(){return Object(d.jsx)(S,{children:Object(d.jsxs)("div",{children:[Object(d.jsx)(c.b,{target:"_blank",to:"//ca.linkedin.com/in/cliffordokpai",children:Object(d.jsxs)(A,{className:"ui linkedin button",children:[Object(d.jsx)("i",{className:"linkedin icon"}),"LinkedIn"]})}),Object(d.jsx)(c.b,{target:"_blank",to:"//github.com/C-N-O",children:Object(d.jsxs)(A,{className:"ui github button",children:[Object(d.jsx)("i",{className:"github icon"}),"Git Hub"]})}),Object(d.jsx)("a",{href:"mailto:cliffordokpai@yahoo.ca",children:Object(d.jsxs)(A,{style:{backgroundColor:"#7934f7",color:"#ffffff"},className:"ui email button",children:[Object(d.jsx)("i",{className:"envelope outline icon"}),"Email"]})})]})})}const T=r.a.div`
+  background: radial-gradient(#d3d8a7, #64ac76, #413670);
+  /* height: 120vh; */
+  overflow-y: scroll;
+`,M=r.a.div`
+  display: grid;
+  grid-template-columns: repeat(2, 50%);
+  grid-template-rows: repeat(5, 50%);
+  height: 85vh;
+`,P=r.a.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`,R=r.a.div`
+  background-color: transparent;
+  display: grid;
+
+  .button {
+    background-color: seashell;
+    &:hover {
+      color: white;
+      background-color: rgb(78, 80, 80);
+    }
+  }
+`,_=r.a.div`
+  background-color: seashell;
+  display: grid;
+
+  .button {
+    background-color: rgb(122, 176, 116);
+
+    &:hover {
+      color: white;
+      background-color: rgb(78, 80, 80);
+    }
+  }
+`,J=r.a.h2`
+  font-size: 2rem;
+  text-align: center;
+  font-family: 'Montserrat', sans-serif;
+`,D=r.a.p`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+  text-align: center;
+`;function E(){return Object(d.jsx)(T,{children:Object(d.jsxs)(M,{children:[Object(d.jsx)(_,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"ARMAST"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-armast.netlify.com",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"View Project"}),Object(d.jsx)("div",{className:"hidden content",children:"Click Now"})]})}),Object(d.jsx)(D,{children:"This is a hub for Java Script Arrays, Math, and String methods."})]})}),Object(d.jsx)(R,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Cliff Blog"}),Object(d.jsx)(c.b,{target:"_blank",to:"//ec2-15-223-46-226.ca-central-1.compute.amazonaws.com/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"See Project"}),Object(d.jsx)("div",{className:"hidden content",children:"Explore"})]})}),Object(d.jsx)(D,{children:"A full stack Blog website with a React front-end, NodeJS and MongoDB back-end, and hosted on Amazon AWS."})]})}),Object(d.jsx)(R,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Online Coffee Shop"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-coffeeshop.netlify.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Click to Enter"}),Object(d.jsx)("div",{className:"hidden content",children:"Visit Store"})]})}),Object(d.jsx)(D,{children:"This is a online coffee store where customers can add to cart and reach checkout. This is still a work in progress."})]})}),Object(d.jsx)(_,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"xChanger"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-xChanger.netlify.com",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Take A Peek"}),Object(d.jsx)("div",{className:"hidden content",children:"Get In"})]})}),Object(d.jsx)(D,{children:"This is a currency converter App using a third-party API."})]})}),Object(d.jsx)(_,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Tip Calculator"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-tipcalc.netlify.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Click Here"}),Object(d.jsx)("div",{className:"hidden content",children:"Explore App"})]})}),Object(d.jsx)(D,{children:"Tip Calculator is used for calculating an order total given the initial amount and a tip."})]})}),Object(d.jsx)(R,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"sQuiz App"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-quizapp.netlify.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"You Think you are smart?"}),Object(d.jsx)("div",{className:"hidden content",children:"Take A Quiz"})]})}),Object(d.jsx)(D,{children:"This is a online quiz App built with HTML, CSS, Vanilla Javascript"})]})}),Object(d.jsx)(R,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Quotes App"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-quotes-app.netlify.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Want to relax and read some quotes?"}),Object(d.jsx)("div",{className:"hidden content",children:"Check this out"})]})}),Object(d.jsx)(D,{children:"This is React App which plays quotes as a slide show. It uses axios to fetch quotes from a thrid party API, Redux to manage state, IdleTimer to monitor activity."})]})}),Object(d.jsx)(_,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Counter App"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-counterapp.netlify.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Want to explore this App?"}),Object(d.jsx)("div",{className:"hidden content",children:"Just Do It"})]})}),Object(d.jsx)(D,{children:"Counter App allows you to select a plain .txt file or you can simply copy and paste. It then tells you the number of words and paragraphs contained in the file/text."})]})}),Object(d.jsx)(_,{children:Object(d.jsxs)(P,{children:[Object(d.jsx)(J,{children:"Disney Plus Clone"}),Object(d.jsx)(c.b,{target:"_blank",to:"//cliffdev-disney-plus-clone.web.app/",children:Object(d.jsxs)("div",{className:"ui animated fade button",tabindex:"0",children:[Object(d.jsx)("div",{className:"visible content",children:"Are you a Star Wars fan?"}),Object(d.jsx)("div",{className:"hidden content",children:"See Disney Plus Clone"})]})}),Object(d.jsx)(D,{children:"This is a Disney Plus Clone. Tech stack includes React, Redux, Styled Components, Firebase Auth and Hosting, React Slick for Sliders, and React Router Dom."})]})})]})})}var q=function(){return Object(d.jsxs)(c.a,{children:[Object(d.jsxs)("div",{children:[Object(d.jsx)(I,{}),Object(d.jsxs)(n.c,{children:[Object(d.jsx)(n.a,{path:"/",exact:!0,component:w}),Object(d.jsx)(n.a,{path:"/about",component:h}),Object(d.jsx)(n.a,{path:"/tech-summary",component:O}),Object(d.jsx)(n.a,{path:"/projects",component:E})]})]}),Object(d.jsx)(C,{})]})};a.a.render(Object(d.jsx)(q,{}),document.getElementById("root"))}},[[37,1,2]]]);
+//# sourceMappingURL=main.54d1d73c.chunk.js.map
